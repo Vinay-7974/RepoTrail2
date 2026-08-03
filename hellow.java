8dbdb37403348cdb2e0e1cd12e79371803537891
vinay chandravanshi <hirevinaychandravanshi@gmail.com>
2026-08-03 18:06:07 +0530

    java code new branch

---
+++ hellow.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public static void main(String[] args) {
+ int arr[] = {2,4,9,7,6,18};
+
+
+    for(int i = 0 ; i<arr.length ; i++){
+        for(int j = i ; j<arr.length; j++){
+            for(int k = i ; k<=j ; k++){
+                System.out.print(arr[k]);
+                
+            }
+        }
+
+    }
+}
